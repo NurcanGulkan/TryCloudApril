39387811a88d0f28261d2a6db956bef482162b28
NurcanGulkan <nurgulkan1115@gmail.com>
2023-03-31 14:59:09 -0400

    project created

---
+++ src/test/java/tryCLoud/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,36 @@
+package tryCLoud.step_definitions;
+
+import io.cucumber.java.en.*;
+import tryCLoud.pages.LoginPage;
+import tryCLoud.utilities.ConfigurationReader;
+import tryCLoud.utilities.Driver;
+
+public class LoginStepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Given("use on the login page")
+    public void use_on_the_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+    }
+
+    @When("user enter valid {string} and {string}")
+    public void userEnterValidAnd(String username, String password) {
+        loginPage.username.sendKeys(username);
+        loginPage.password.sendKeys(password);
+}
+
+
+    @When("user click login button")
+    public void user_click_login_button() {
+       loginPage.loginButton.click();
+
+    }
+    @Then("user should be at dashboard page")
+    public void user_should_be_at_dashboard_page() {
+
+    }
+
+
+}

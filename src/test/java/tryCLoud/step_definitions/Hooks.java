5a33483a76bd2459a6adf5b5f7f0934d4ad5ab41
NurcanGulkan <nurgulkan1115@gmail.com>
2023-04-05 13:50:18 -0400

    hooks class added

--- src/test/java/tryCLoud/step_definitions/Hooks.java
+++ src/test/java/tryCLoud/step_definitions/Hooks.java
@@ -1,4 +1,27 @@
 package tryCLoud.step_definitions;
 
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import tryCLoud.utilities.ConfigurationReader;
+import tryCLoud.utilities.Driver;
+
 public class Hooks {
+    @Before
+    public void setUpScenario(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+    @After
+    public void teardownScenario(Scenario scenario){
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+        Driver.closeDriver();
+    }
 }
